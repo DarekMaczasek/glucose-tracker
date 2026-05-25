65b3060348adbf371cbb82d7b530eb334b0695ca
DarekMaczasek <51539719+DarekMaczasek@users.noreply.github.com>
2026-05-25 15:15:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const DATA_FILE = './data.json';
 
